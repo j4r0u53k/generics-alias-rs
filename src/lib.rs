d5f37c4c348c058d15592eea8824b4c02a8d4f0b
Jaroslav Beran <jara.beran@gmail.com>
2024-02-18 23:44:32 +0100

    Add docs with some examples
    (#2, #3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,55 @@
+//! # Generics alias
+//!
+//! The crate provides macros for creating an alias of some generics that can be reused
+//! on functions or struct/trait impls without repeating the whole definition.
+//!
+//! ## Examples
+//!
+//! Making an alias for multiple trait bounds:
+//!
+//! ```
+//! use generics_alias::*;
+//!
+//! generics_def!(ManyBounds
+//!     <
+//!         F: FnOnce(Y) -> Z,
+//!         Y: Copy,
+//!         Z: Into<Option<Y>>,
+//!     >
+//! );
+//!
+//! #[generics(ManyBounds)]
+//! fn foo(f: F, y: Y) -> Z {
+//!    f(y)
+//! }
+//! ```
+//!
+//! Use on a struct/trait and impl block:
+//!
+//! ```
+//! use core::fmt::Debug;
+//! use generics_alias::*;
+//!
+//! generics_def!(DebugClonable <X: Debug + Clone>);
+//!
+//! #[generics(DebugClonable)]
+//! struct Foo { x: X }
+//!
+//! #[generics(DebugClonable)]
+//! impl Foo<X> { }
+//!
+//! #[generics(DebugClonable)]
+//! trait Bar { }
+//!
+//! #[generics(DebugClonable)]
+//! impl Bar<X> for i32 { }
+//! ```
+//!
+
 // NOTE: The reexport of `macro_magic` crate does not work (I do not know why), so users still
 // have to specify the dependency in their Cargo.toml :(
 pub extern crate macro_magic;
+#[doc(hidden)]
 pub use macro_magic::*;
 pub use generics_alias_macros::*;
 
